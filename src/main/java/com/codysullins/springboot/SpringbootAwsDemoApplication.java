daa5b9edb75a6b25f94b6bb7af9b80de10977061
salhounz <codyjsullins@gmail.com>
2023-08-23 10:48:20 -0500

    Specifying dev (development) branch

--- src/main/java/com/codysullins/springboot/SpringbootAwsDemoApplication.java
+++ src/main/java/com/codysullins/springboot/SpringbootAwsDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootAwsDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("This is in dev");
 		SpringApplication.run(SpringbootAwsDemoApplication.class, args);
 	}
 
