a65c7e04c305cd9fb98d59e244083131de4a7ad4
salhounz <codyjsullins@gmail.com>
2023-08-23 10:42:35 -0500

    Initial project commit

---
+++ src/test/java/com/codysullins/springboot/SpringbootAwsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codysullins.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
